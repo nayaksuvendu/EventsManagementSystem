8ad0ab4a7b3900ff7ca21f2b1e4e0a5deca005f5
SUVENDU KUMAR NAYAK <139025660+nayaksuvendu@users.noreply.github.com>
2024-09-28 11:10:21 +0530

    bug fix

--- client/src/components/authfirebase/Firebase.js
+++ client/src/components/authfirebase/Firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // Your web app's Firebase configuration
     const firebaseConfig = {
-      apiKey: "AIzaSyCMdYn0GQbaGl_uiSqIeP4eIxe2S5TxYG4",
-      authDomain: "nayakevents-fe8d6.firebaseapp.com",
-      projectId: "nayakevents-fe8d6",
-      storageBucket: "nayakevents-fe8d6.appspot.com",
-      messagingSenderId: "779844306150",
-      appId: "1:779844306150:web:f6e69ff969a25031ff2727",
-      measurementId: "G-JQ3R05Z98P"
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     };
 
 // Initialize Firebase
